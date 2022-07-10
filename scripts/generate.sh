043f92cdce021591ac4b25b6a94bcdb1982fa3ed
J. Zebedee <zebedee@code.gripe>
2022-07-10 06:56:30 -0500

    Create Bannerlord directory structure

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
+
+mkdir -p Native/Videos
+cd Native/Videos
+
 ffmpeg -t 0.02 -f lavfi -i color=c=black:s=1920x1080 -c:v vp8 -pix_fmt yuv420p TWLogo_and_Partners.ivf
-ffmpeg -f lavfi -i anullsrc -t 0.02 -c:a libvorbis TWLogo_and_Partners.ogg
+ffmpeg -t 0.02 -f lavfi -i anullsrc -c:a libvorbis TWLogo_and_Partners.ogg
 
 ffmpeg -t 0.02 -f lavfi -i color=c=black:s=1920x1080 -c:v libx264 -pix_fmt yuv420p TWLogo_and_Partners.mp4
