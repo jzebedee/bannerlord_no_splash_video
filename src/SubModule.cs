cca13da144c562703fc2bd3fbe0505757e6e6d59
J. Zebedee <zebedee@code.gripe>
2022-07-14 10:29:13 -0500

    Add submodule to skip splash videos programmatically

---
+++ src/SubModule.cs
@@ -0,0 +1,33 @@
+﻿using HarmonyLib;
+using System;
+using TaleWorlds.MountAndBlade;
+
+namespace SkipSplashVideos
+{
+    public class SubModule : MBSubModuleBase
+    {
+        private const string HarmonyId = $"{nameof(SkipSplashVideos)}.harmony";
+
+        private readonly Lazy<Harmony> _harmony = new(() => new Harmony(HarmonyId));
+
+        protected override void OnSubModuleLoad()
+        {
+            base.OnSubModuleLoad();
+
+            _harmony.Value.PatchAll();
+        }
+
+        protected override void OnSubModuleUnloaded()
+        {
+            base.OnSubModuleUnloaded();
+
+            _harmony.Value.UnpatchAll(HarmonyId);
+        }
+
+        protected override void OnBeforeInitialModuleScreenSetAsRoot()
+        {
+            base.OnBeforeInitialModuleScreenSetAsRoot();
+
+        }
+    }
+}
